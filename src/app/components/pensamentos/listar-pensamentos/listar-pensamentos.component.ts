0f276d99e1ad419ce85b140f8b59d37ddd934f09
Antonio Eduardo Garcia Moreira <tonyeg2020@gmail.com>
2022-11-16 17:49:23 -0300

    NgFor, comunicação entre componentes, NgIf, NgClass

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamentos',
   templateUrl: './listar-pensamentos.component.html',
-  styleUrls: ['./listar-pensamentos.component.css']
+  styleUrls: ['./listar-pensamentos.component.css'],
 })
 export class ListarPensamentosComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Teste',
+      autoria: 'DEV NELES',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Teste22',
+      autoria: 'DEV NELES222',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo:
+        'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
+      autoria: 'DEV NELES222',
+      modelo: 'modelo1',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
