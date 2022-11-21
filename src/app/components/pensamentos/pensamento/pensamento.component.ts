6edf69ab960c0f2c108d02b743df3dd7b8328c9f
Antonio Eduardo Garcia Moreira <tonyeg2020@gmail.com>
2022-11-21 17:37:47 -0300

    finished favorite button/icon funcionalits

--- src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
@@ -15,7 +16,7 @@ export class PensamentoComponent implements OnInit {
     favorito: false,
   };
 
-  constructor() {}
+  constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {}
 
@@ -32,5 +33,9 @@ export class PensamentoComponent implements OnInit {
     } else {
       return 'ativo';
     }
+  }
+
+  atualizarFavoritos() {
+    this.service.mudarFavorito(this.pensamento).subscribe();
   }
 }
